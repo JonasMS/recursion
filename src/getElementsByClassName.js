6ece77e79020e1c807d34cc13202949936ea2d5e
JonasMS <!!Exodus47!>
2016-03-21 18:37:16 -0700

    Complete getElementsByClassName lab

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,35 @@
 //   return document.getElementsByClassName(className);
 // };
 
+/*
+	Problem: return an array of all html element has have the given class name
+	Solution:
+		*document.body, element.childNodes, and element.classList
+	A: element has class
+	B: search element's child nodes
+*/
+
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+	var choiceEls = [];
+	// var elements;
+	var els;
+	var body = document.body;
+
+	var searchEl = function(element){
+		// elements = [element].push(element.childNodes);
+		var elements = element.childNodes;
+
+
+		for(var i = 0; i < elements.length; i++){
+			if(_.contains(elements[i].classList, className)){ choiceEls.push(elements[i]); }
+			searchEl(elements[i]);
+		}
+	}
+
+	if(_.contains(body.classList, className)){ choiceEls.push(body); }
+	searchEl(body);
+
+	return choiceEls;
+
 };
